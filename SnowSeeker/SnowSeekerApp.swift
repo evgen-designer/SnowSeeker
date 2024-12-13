1fed699aa1a128458aa54a2d7e0b123a2d13bdce
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-12-13 07:44:56 +0000

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Mac on 13/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
